70548534b858e926ef1248ea04233d15ae5efba9
Balasankar C <balasankarc@autistici.org>
2015-10-25 12:32:43 +0530

    Add group support

--- gemfileparser.py
+++ gemfileparser.py
@@ -7,11 +7,12 @@ import re
 
 class GemfileParser:
 
+    GROUP = 'runtime'
+
     class Dependency:
 
         def __init__(self):
             self.name = ''
-            self.reqtype = ''
             self.requirement = ''
             self.autorequire = ''
             self.source = ''
@@ -19,17 +20,19 @@ class GemfileParser:
             self.group = ''
 
         def __str__(self):
-            return self.name
+            return self.name + ", " + self.requirement
 
     gemname_regex = re.compile(r"(?P<gemname>[a-zA-Z]+[0-9a-zA-Z _-]*)")
     req_regex = re.compile(r"(?P<reqs>([>|<|=|~>|\d]+[ ]*[0-9\.\w]+[ ,]*)+)")
     source_regex = re.compile(r"source:[ ]?(?P<source>[a-zA-Z:\/\.-]+)")
     autoreq_regex = re.compile(r"require:[ ]?(?P<autoreq>[a-zA-Z:\/\.-]+)")
     group_regex = re.compile(r"group:[ ]?(?P<groupname>[a-zA-Z:\/\.-]+)")
+    group_block_regex = re.compile(r"group[ ]?:[ ]?(?P<groupblock>.*) do")
 
     def __init__(self, filepath):
         self.gemfile = open(filepath)
-        self.dependencies = []
+        self.dependencies = {'development': [],
+                             'runtime': [], 'test': [], 'production': []}
         self.contents = self.gemfile.readlines()
         if filepath.endswith('gemspec'):
             self.gemspec = True
@@ -37,12 +40,16 @@ class GemfileParser:
             self.gemspec = False
 
     def parse(self):
-
-        dependencies = []
         for line in self.contents:
             line = line.strip()
             if line.startswith('source') or line.startswith('#'):
                 continue
+            elif line.startswith('group'):
+                match = self.group_block_regex.match(line)
+                if match:
+                    self.GROUP = match.group('groupblock')
+            elif line.startswith('end'):
+                self.GROUP = 'runtime'
             elif line.startswith('gem'):
                 line = line[3:]
                 linefile = StringIO.StringIO(line)
@@ -54,6 +61,7 @@ class GemfileParser:
                             l = l[:l.index("#")]
                         m.append(l)
                     dep = self.Dependency()
+                    dep.group = self.GROUP
                     for k in m:
                         match = self.source_regex.match(k)
                         if match:
@@ -79,9 +87,16 @@ class GemfileParser:
                                 dep.requirement += ' ' + match.group('reqs')
                             continue
                     dep.parent = 'diaspora'
-                    dependencies.append(dep)
-        return dependencies
+                    if self.GROUP in self.dependencies:
+                        self.dependencies[self.GROUP].append(dep)
+                    else:
+                        self.dependencies[self.GROUP] = [dep]
+        return self.dependencies
 
 if __name__ == "__main__":
     n = GemfileParser('Gemfile')
-    n.parse()
+    deps = n.parse()
+    for key in deps:
+        print key
+        for dependency in deps[key]:
+            print "\t", dependency
